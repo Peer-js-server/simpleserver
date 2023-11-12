92d37b167bc2feece561161d1156da4b182ee97d
Peer-js-server <150596884+Peer-js-server@users.noreply.github.com>
2023-11-12 15:18:02 +0100

    d

---
+++ index.js
@@ -0,0 +1,19 @@
+const { PeerServer} = require("peer");
+
+const PORT = 9002;
+
+const peerServer = PeerServer({
+    port: PORT,
+    key:"demodemo",
+    path: "/myapp",
+    allow_discovery: true,
+});
+
+peerServer.on("connection", c => {
+    console.log("connection ID:", c.id)
+})
+
+peerServer.on("disconnect", c=> {
+    console.log("disconnect!", c.id)
+})
+console.log(`Running Peer Js server on por ${PORT}`);
